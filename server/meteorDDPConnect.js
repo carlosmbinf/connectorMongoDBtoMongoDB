c3f1482f09f343df5f0f0594323d51b434990b4d
Carlos Medina <carlosmbinf@gmail.com>
2021-05-20 20:01:28 -0400

    arreglos

--- server/meteorDDPConnect.js
+++ server/meteorDDPConnect.js
@@ -1,6 +1,7 @@
 const simpleDDP = require("simpleddp"); // nodejs
 const ws = require("isomorphic-ws");
 const simpleDDPLogin = require("simpleddp-plugin-login").simpleDDPLogin;
+const { Binary, ObjectID } = require('mongodb')
 
 export async function conectMeteor() {
   //   let meteor1 = await DDP.connect("ws://localhost:3020/websocket");
@@ -26,42 +27,66 @@ export async function conectMeteor() {
     user: "admin@vr.com",
     password: "duiTqmYGWagkXCwrm",
   };
-  server1.on("connected", () => {
-    // do something
-    console.log("server1 connected: " + server1.connected);
-
-    try {
-      (async () => {
-        //       // await server1.subscribe("Members").ready()
-        //       // await server1.subscribe("external.oldMembers.all",credential).ready()
-        //       // await server1.subscribe("external.employees.all",credential).ready()
-        //       // await server1.subscribe("external.employeesNotUser.all",credential).ready()
-        //       // await server1.subscribe("Folios",credential).ready()
-        //       // await server1.subscribe("external.users.all",credential).ready()
-        //       // await server1.subscribe("external.club",credential).ready()
-        //       // await server1.subscribe("external.annualFee.all",credential).ready()
-        //       // await server1.subscribe("external.services.all",credential).ready()
-        //       // await server1.subscribe("external.specialIncentives.all",credential).ready()
-        //       // await server1.subscribe("external.commissions.all",credential).ready()
-        //       // await server1.subscribe("external.connections.all",credential).ready()
-        //       // await server1.subscribe("external.documents.all",credential).ready()
-        //       // await server1.subscribe("external.letters.all",credential).ready()
-        //       // await server1.subscribe("external.membershipTypes.all",credential).ready()
-        //       // // await server1.subscribe("external.files.agreements.all",credential)
-        //       // // await server1.subscribe("external.files.scannedAgreements.all",credential)
-        //       // await server1.subscribe("external.Attendee.all",credential).ready()
-        //       // await server1.subscribe("external.log.all",credential).ready()
+  server1.on("connected",async () => {
+    // do something 
+    await console.log("server1 connected: " + server1.connected);
+    await server1.subscribe("files.agreements.all").ready();
+    await server1.subscribe("files.scannedAgreements.all").ready();
+    await server1.subscribe("files.images.all").ready();
+    await server1.subscribe("fs.chunk.all").ready();
+    await console.log("Subscrito a FILES ALL ( agreements scannedAgreements  images )");
     
-        //       // console.log(await JSON.parse(JSON.stringify(server1.collection()._server1.collections)));
-        //       // server1.call("getMembersAll").then((a) => console.log(a));
-        //   //all subs are ready here
-        //  server1.subscribe("external.members.all", credential)
-         await server1.subscribe("external.members.all", credential).ready();
-         console.log("Subscrito a external.members.all");
-         
-         await server2.disconnect();
-         await server2.connect();
-        })();
+    await server1.subscribe("Members").ready();
+    await console.log("Subscrito a Members");
+
+    
+    await console.log("Subscrito a Services");
+    await server1.subscribe("Comissions").ready();
+
+    await server1.subscribe("Documents").ready();
+    await console.log("Subscrito a Documents");
+    await server1.subscribe("AnnualFees").ready();
+    await console.log("Subscrito a AnnualFees");
+    await server1.subscribe("Services").ready();
+    await console.log("Subscrito a Comissions");
+    await server1.subscribe("MembershipTypes").ready();
+    await console.log("Subscrito a MembershipTypes");
+    try {
+      // await server1.subscribe('file.agreements.all').ready()
+      // await console.log("Subscrito a file.agreements.all");
+      // await server1.subscribe('files.scannedAgreements.all').ready()
+      // await console.log("Subscrito a files.scannedAgreements.all");
+
+      //       // await server1.subscribe("Members").ready()
+      //       // await server1.subscribe("external.oldMembers.all",credential).ready()
+      //       // await server1.subscribe("external.employees.all",credential).ready()
+      //       // await server1.subscribe("external.employeesNotUser.all",credential).ready()
+      //       // await server1.subscribe("Folios",credential).ready()
+      //       // await server1.subscribe("external.users.all",credential).ready()
+      //       // await server1.subscribe("external.club",credential).ready()
+      // await server1.subscribe("AnnualFees").ready()
+      //       // await server1.subscribe("external.services.all",credential).ready()
+      //       // await server1.subscribe("external.specialIncentives.all",credential).ready()
+      //       // await server1.subscribe("external.commissions.all",credential).ready()
+      //       // await server1.subscribe("external.connections.all",credential).ready()
+      //       // await server1.subscribe("external.documents.all",credential).ready()
+      //       // await server1.subscribe("external.letters.all",credential).ready()
+      //       // await server1.subscribe("external.membershipTypes.all",credential).ready()
+      //       // // await server1.subscribe("external.files.agreements.all",credential)
+      //       // // await server1.subscribe("external.files.scannedAgreements.all",credential)
+      //       // await server1.subscribe("external.Attendee.all",credential).ready()
+      //       // await server1.subscribe("external.log.all",credential).ready()
+
+      //       // console.log(await JSON.parse(JSON.stringify(server1.collection()._server1.collections)));
+      //       // server1.call("getMembersAll").then((a) => console.log(a));
+      //   //all subs are ready here
+      //  server1.subscribe("external.members.all", credential)
+      //  await server1.subscribe("external.members.all", credential).ready();
+      //  await console.log("Subscrito a external.members.all");
+      //  await console.log("Subscrito a AnnualFees");
+
+      await server2.disconnect();
+      await server2.connect();
     } catch (error) {
       console.log(error);
     }
@@ -114,26 +139,90 @@ export async function conectMeteor() {
   server2.on("login", async (m) => {
     await console.log("User logged in as", m);
     try {
-      await server1.call("getMembersAll", credential).then((members) => {
-        members.forEach((member) => {
-          member &&
-            server2
-              .call("externalInsertMember", credential, member)
-              .then(() => {});
-        });
+      // await server1.call("getMembersAll", credential).then((members) => {
+      //   members.forEach((member) => {
+      //     member &&
+      //       server2
+      //         .call("externalInsertMember", credential, member)
+      //         .then(() => {});
+      //   });
+      //   // console.log(server2.call("externalLogin", autentication));
+      // });
+    
+      ////////////////// getAgreementsAll ///////////////////
+      // await server1.call("getAgreementsAll").then((data) => {
+      //   data.forEach((Agreement) => {
+      //     Agreement &&
+      //       server2
+      //         .call("setAgreementsAll", Agreement)
+      //         .then(() => {});
+      //     console.log(Agreement);
+      //   });
+
+      //   // console.log(server2.call("externalLogin", autentication));
+      // });
+
+      //  ////////////////// getScannedAgreementsAll ///////////////////
+      //  await server1.call("getScannedAgreementsAll").then((data) => {
+      //   data.forEach((ScannedAgreement) => {
+      //     ScannedAgreement &&
+      //       server2
+      //         .call("setScannedAgreementsAll", ScannedAgreement)
+      //         .then(() => {});
+      //     console.log(ScannedAgreement);
+      //   });
+
+      //   // console.log(server2.call("externalLogin", autentication));
+      // });
+
+      //  ////////////////// getImagesAll ///////////////////
+      //  await server1.call("getImagesAll").then((data) => {
+      //   data.forEach((Image) => {
+      //     Image &&
+      //       server2
+      //         .call("setImagesAll", Image)
+      //         .then(() => {});
+      //     console.log(Image);
+      //   });
+      //   // console.log(server2.call("externalLogin", autentication));
+      // });
+
+
+        await server1.call("getchunksAll").then(async (data) => {
+          try {
+          await  JSON.parse(data).forEach((chunk) => {
+            // console.log(chunk._id);
+            // console.log(chunk.data);
+
+            console.log(Binary(Array(chunk.data)));
+            // chunk.data = new Binary(Array(chunk.data))
+            // console.log(chunk._id);
+
+              chunk &&
+                server2
+                  .call("setchunksAll", chunk).then((data)=>{console.log('terminado');});
+            });
+            // server2
+            //       .call("setchunksAll", data).then(()=>{console.log('terminado');});
+              
+            // console.log(data);
+          } catch (e) {
+            console.log(e);
+          }
+      
+
         // console.log(server2.call("externalLogin", autentication));
       });
     } catch (error) {
       console.log(error);
     }
   });
-
   server2.on("disconnected", () => {
     console.log("Disconnected server2");
    
   });
   server2.on("ready", () => {
-    console.log("Disconnected server2");
+    console.log("ready server2");
    
   });
   
